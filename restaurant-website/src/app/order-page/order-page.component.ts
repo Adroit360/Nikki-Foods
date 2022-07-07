f3cd192e493801529ad794ac9e0fbafae5e3a9f3
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-07 08:47:33 +0000

    done withe project

--- restaurant-website/src/app/order-page/order-page.component.ts
+++ restaurant-website/src/app/order-page/order-page.component.ts
@@ -179,8 +179,8 @@ export class OrderPageComponent implements OnInit {
 
     this.loading = true;
     const body = {
-      // amount: this.totalPrice * 100,
-      amount: 0.01 * 100,
+      amount: this.totalPrice * 100,
+      // amount: 0.01 * 100,
       clientId: this.clientTransactionId,
       orderDetails: this.orderDetails,
     };
